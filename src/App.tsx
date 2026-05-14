729ed682a801d76bd29fc5b2ee691cb9b87c07e9
Vas. Neratzis <vas.neratzis@gmail.com>
2026-05-14 15:08:54 +0300

    feat: add ArrowFunctionalComponentWithProps

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,66 @@
-import FunctionalComponent   from "./components/LessonTwo/FunctionalComponent.tsx";
-import ClassComponent from "./components/LessonTwo/ClassComponent.tsx";
-import ArrowFunctionalComponent from "./components/LessonTwo/ArrowFunctionalComponent.tsx";
+// import FunctionalComponent   from "./components/LessonTwo/FunctionalComponent.tsx";
+// import ClassComponent from "./components/LessonTwo/ClassComponent.tsx";
+// import ArrowFunctionalComponent from "./components/LessonTwo/ArrowFunctionalComponent.tsx";
+
+// type PropsA = {
+//     title: string;
+//     description: string;
+// }
+//
+// type PropsB = {
+//     title: string;
+//     description: string;
+// }
+//
+//
+// // Union
+// type Status = "Error" | "Info" | "Warning";
+//
+// // Tuples
+// type Coordinates = [number, number];
+//
+// //Functions
+// type ClickHandler = (event: MouseEvent) => void;
+//
+// //Intersection
+//
+// type Props = PropsA & PropsB;
+//
+// interface PropsTwo {
+//     title: string;
+//     description: string;
+// }
+//
+// interface PropsTwo {
+//     price: number,
+//     sort: number;
+// }
+//
+// //PropsTwo = {title, description, price,sort}
+//
+// interface PropsThree extends PropsTwo {
+//     image: string,
+// }
+
+//propsThree = {title, description,price,sort,image}
+
+import ArrowFunctionalComponentWithProps from "./components/LessonThree/ArrowFunctionalComponentWithProps.tsx";
 
 function App() {
 
   return (
     <>
-        <FunctionalComponent />
-        <ClassComponent />
-        <ArrowFunctionalComponent />
+        {/*<FunctionalComponent />*/}
+        {/*<ClassComponent />*/}
+        {/*<ArrowFunctionalComponent />*/}
 
+        <ArrowFunctionalComponentWithProps title="Heading 1"/>
+        <ArrowFunctionalComponentWithProps
+            title="Second Title"
+            description="This is the description for second title" />
+
+        <h2 className="cf-text" id="id"></h2>
+        {/*h2.cf-text#id */}
       </>
   )
 }
