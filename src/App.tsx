ce2cbb8865962a9349427b0b0c8c68d8ee5a08a3
Vas. Neratzis <vas.neratzis@gmail.com>
2026-05-19 19:19:27 +0300

    feat: add AutoReDirect component

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import Layout from "./components/Layout.tsx";
 // import CounterAdvanced from "./components/LessonFour/CounterAdvanced.tsx";
 // import NameChanger from "./components/LessonFive/NameChanger.tsx";
 // import CounterWithCustomHook from "./components/LessonFive/CounterWithCustomHook.tsx";
-import NameChangerWithTitle from "./components/LessonFive/NameChangerWithTitle.tsx";
+// import NameChangerWithTitle from "./components/LessonFive/NameChangerWithTitle.tsx";
+import AutoRedirect from "./components/LessonSix/AutoRedirect.tsx";
 // import {useEffect} from "react";
 // import Counter from "./components/LessonFour/Counter.tsx";
 // import ClassCounter from "./components/LessonFour/ClassCounter.tsx";
@@ -97,12 +98,18 @@ function App() {
             {/*</Layout>*/}
 
             {/*Lesson Five*/}
+            {/*<Layout addClasses="bg-gray-50">*/}
+            {/*/!*<NameChanger/>*!/*/}
+            {/*/!*    <CounterWithCustomHook/>*!/*/}
+            {/*    <NameChangerWithTitle/>*/}
+            {/*</Layout>*/}
+
+
+        {/* Lesson Six*/}
             <Layout addClasses="bg-gray-50">
-            {/*<NameChanger/>*/}
-            {/*    <CounterWithCustomHook/>*/}
-                <NameChangerWithTitle/>
+                <AutoRedirect/>
             </Layout>
-        </>
+</>
     )
 }
 
