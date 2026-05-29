6d3b918d0efb315312abbe233f111f973fe12470
Vas. Neratzis <vas.neratzis@gmail.com>
2026-05-29 20:43:32 +0300

    chore: add button to HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,10 @@
+import {Button} from "../components/ui/button.tsx";
+
 const HomePage = () =>{
     return (
         <>
             <h1>Home Page</h1>
+            <Button>Button</Button>
         </>
     )
 }
